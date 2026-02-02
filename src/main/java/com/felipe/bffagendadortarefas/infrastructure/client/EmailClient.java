95af86f897c1a201d652ae947e40e3fe1e7e0a3c
Felipe Diniz <felipedinizdev@gmail.com>
2026-02-02 18:07:07 -0300

    adicionado client de email

---
+++ src/main/java/com/felipe/bffagendadortarefas/infrastructure/client/EmailClient.java
@@ -0,0 +1,16 @@
+package com.felipe.bffagendadortarefas.infrastructure.client;
+
+
+
+import com.felipe.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "notificacao", url = "${notificacao.url}")
+public interface EmailClient {
+
+    @PostMapping
+    void enviarEmail(@RequestBody TarefasDTOResponse dto) ;
+}
